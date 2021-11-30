b6423a573d43ffa02e45f5331357092d7afa5015
Lindsey <84094288+lindseyfitzgerald@users.noreply.github.com>
2021-11-30 19:45:42 -0330

    Display page completed and upload func updated

--- Objects.py
+++
@@ -1,7 +0,0 @@
-class Game:
-    def __init__(self, name=None, image=None, console=None, year=0, description=None):
-        self.name = name
-        self.image = image
-        self.console = console
-        self.year = year
-        self.description = description
